806e3e6b474dd5b6f55eb2a8c810ab76c55856fb
Prem Rajendran <75168548+Pr3mR4jendran@users.noreply.github.com>
2022-08-11 09:57:44 +0400

    Update quine.js

--- quine.js
+++ quine.js
@@ -1,5 +1,1 @@
-(function quine(){
-    console.log('(' + quine.toString() + ')();');
-})();
-(function o(f) { console.log( f(o) + f(f) )}) (f => '(' + f + ')')
 !function $(){console.log('!'+$+'()')}()
